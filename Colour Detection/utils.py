05a0ccea19222ead8af40c661473c01e5c174944
meet4041 <meetgandhi4041@example.com>
2026-03-30 13:02:59 +0530

    Initial Commit

--- Colour Detection/utils.py
+++ Colour Detection/utils.py
@@ -2,13 +2,15 @@ import numpy as np
 import cv2
 
 def get_limits(color):
+    
     c = np.uint8([[color]])
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
-
-    hue = int(hsvC[0][0][0])
-
-    # Use a wider range so real camera lighting still matches the target color.
-    lowerLimit = np.array([max(hue - 20, 0), 50, 50], dtype=np.uint8)
-    upperLimit = np.array([min(hue + 20, 179), 255, 255], dtype=np.uint8)
-
+     
+    lowerLimit = hsvC[0][0][0] - 10, 100, 100
+    upperLimit = hsvC[0][0][0] + 10, 255, 255
+    
+    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
+    upperLimit = np.array(upperLimit, dtype=np.uint8)
+    
     return lowerLimit, upperLimit
+    
